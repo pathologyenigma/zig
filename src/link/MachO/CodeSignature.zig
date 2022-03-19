a58fb664b03c18ee10d4145b1eaf1e81ccbc14f3
Jakub Konka <kubkon@jakubkonka.com>
2022-03-19 16:54:11 +0100

    macho: towards entitlements

--- src/link/MachO/CodeSignature.zig
+++ src/link/MachO/CodeSignature.zig
@@ -12,9 +12,61 @@ const Sha256 = std.crypto.hash.sha2.Sha256;
 
 const hash_size: u8 = 32;
 
+const Blob = union(enum) {
+    code_directory: CodeDirectory,
+    requirements: Requirements,
+    entitlements: Entitlements,
+    signature: Signature,
+
+    fn deinit(self: *Blob, allocator: Allocator) void {
+        switch (self.*) {
+            .code_directory => |*x| x.deinit(allocator),
+            .requirements => |*x| x.deinit(allocator),
+            .entitlements => |*x| x.deinit(allocator),
+            .signature => |*x| x.deinit(allocator),
+        }
+    }
+
+    fn slotType(self: Blob) u32 {
+        return switch (self) {
+            .code_directory => |x| x.slotType(),
+            .requirements => |x| x.slotType(),
+            .entitlements => |x| x.slotType(),
+            .signature => |x| x.slotType(),
+        };
+    }
+
+    fn size(self: Blob) u32 {
+        return switch (self) {
+            .code_directory => |x| x.size(),
+            .requirements => |x| x.size(),
+            .entitlements => |x| x.size(),
+            .signature => |x| x.size(),
+        };
+    }
+
+    fn write(self: Blob, writer: anytype) !void {
+        return switch (self) {
+            .code_directory => |x| x.write(writer),
+            .requirements => |x| x.write(writer),
+            .entitlements => |x| x.write(writer),
+            .signature => |x| x.write(writer),
+        };
+    }
+};
+
 const CodeDirectory = struct {
     inner: macho.CodeDirectory,
     data: std.ArrayListUnmanaged(u8) = .{},
+
+    fn deinit(self: *CodeDirectory, allocator: Allocator) void {
+        self.data.deinit(allocator);
+    }
+
+    fn slotType(self: CodeDirectory) u32 {
+        _ = self;
+        return macho.CSSLOT_CODEDIRECTORY;
+    }
 
     fn size(self: CodeDirectory) u32 {
         return self.inner.length;
@@ -46,6 +98,74 @@ const CodeDirectory = struct {
     }
 };
 
+const Requirements = struct {
+    fn deinit(self: *Requirements, allocator: Allocator) void {
+        _ = self;
+        _ = allocator;
+    }
+
+    fn slotType(self: Requirements) u32 {
+        _ = self;
+        return macho.CSSLOT_REQUIREMENTS;
+    }
+
+    fn size(self: Requirements) u32 {
+        _ = self;
+        return 3 * @sizeOf(u32);
+    }
+
+    fn write(self: Requirements, writer: anytype) !void {
+        try writer.writeIntBig(u32, macho.CSMAGIC_REQUIREMENTS);
+        try writer.writeIntBig(u32, self.size());
+        try writer.writeIntBig(u32, 0);
+    }
+};
+
+const Entitlements = struct {
+    inner: []const u8,
+
+    fn deinit(self: *Entitlements, allocator: Allocator) void {
+        allocator.free(self.inner);
+    }
+
+    fn slotType(self: Entitlements) u32 {
+        _ = self;
+        return macho.CSSLOT_ENTITLEMENTS;
+    }
+
+    fn size(self: Entitlements) u32 {
+        return @intCast(u32, self.inner.len) + 2 * @sizeOf(u32);
+    }
+
+    fn write(self: Entitlements, writer: anytype) !void {
+        try writer.writeIntBig(u32, macho.CSMAGIC_EMBEDDED_ENTITLEMENTS);
+        try writer.writeIntBig(u32, self.size());
+        try writer.writeAll(self.inner);
+    }
+};
+
+const Signature = struct {
+    fn deinit(self: *Signature, allocator: Allocator) void {
+        _ = self;
+        _ = allocator;
+    }
+
+    fn slotType(self: Signature) u32 {
+        _ = self;
+        return macho.CSSLOT_SIGNATURESLOT;
+    }
+
+    fn size(self: Signature) u32 {
+        _ = self;
+        return 2 * @sizeOf(u32);
+    }
+
+    fn write(self: Signature, writer: anytype) !void {
+        try writer.writeIntBig(u32, macho.CSMAGIC_BLOBWRAPPER);
+        try writer.writeIntBig(u32, self.size());
+    }
+};
+
 /// Code signature blob header.
 inner: macho.SuperBlob = .{
     .magic = macho.CSMAGIC_EMBEDDED_SIGNATURE,
@@ -53,8 +173,7 @@ inner: macho.SuperBlob = .{
     .count = 0,
 },
 
-/// CodeDirectory header which holds the hash of the binary.
-cdir: ?CodeDirectory = null,
+blobs: std.ArrayListUnmanaged(Blob) = .{},
 
 pub fn calcAdhocSignature(
     self: *CodeSignature,
@@ -96,23 +215,67 @@ pub fn calcAdhocSignature(
         },
     };
 
-    const total_pages = mem.alignForward(file_size, page_size) / page_size;
+    // 1. Save the identifier and update offsets
+    cdir.inner.identOffset = cdir.inner.length;
+    try cdir.data.appendSlice(allocator, id);
+    try cdir.data.append(allocator, 0);
 
     var hash: [hash_size]u8 = undefined;
+
+    // 2. Create Requirements blob
+    var req: Requirements = .{};
+    cdir.inner.nSpecialSlots = 7;
+    {
+        try cdir.data.appendNTimes(allocator, 0, hash_size);
+    }
+    {
+        try cdir.data.appendNTimes(allocator, 0, hash_size);
+    }
+
+    // 3. Create Entitlements blob
+    const path = "/Users/kubkon/dev/zig/resources/Info.plist";
+    const ff = try std.fs.cwd().openFile(path, .{});
+    defer ff.close();
+    var inner = try ff.readToEndAlloc(allocator, std.math.maxInt(u32));
+    var ent: Entitlements = .{
+        .inner = inner,
+    };
+    {
+        var buf = std.ArrayList(u8).init(allocator);
+        defer buf.deinit();
+        try ent.write(buf.writer());
+        Sha256.hash(buf.items, &hash, .{});
+        try cdir.data.appendSlice(allocator, &hash);
+    }
+    {
+        try cdir.data.appendNTimes(allocator, 0, hash_size);
+    }
+    {
+        try cdir.data.appendNTimes(allocator, 0, hash_size);
+    }
+
+    {
+        var buf = std.ArrayList(u8).init(allocator);
+        defer buf.deinit();
+        try req.write(buf.writer());
+        Sha256.hash(buf.items, &hash, .{});
+        try cdir.data.appendSlice(allocator, &hash);
+    }
+    {
+        try cdir.data.appendNTimes(allocator, 0, hash_size);
+    }
+
+    const total_pages = mem.alignForward(file_size, page_size) / page_size;
+
     var buffer = try allocator.alloc(u8, page_size);
     defer allocator.free(buffer);
 
-    try cdir.data.ensureTotalCapacityPrecise(allocator, total_pages * hash_size + id.len + 1);
-
-    // 1. Save the identifier and update offsets
-    cdir.inner.identOffset = cdir.inner.length;
-    cdir.data.appendSliceAssumeCapacity(id);
-    cdir.data.appendAssumeCapacity(0);
+    try cdir.data.ensureUnusedCapacity(allocator, total_pages * hash_size);
 
     // 2. Calculate hash for each page (in file) and write it to the buffer
     // TODO figure out how we can cache several hashes since we won't update
     // every page during incremental linking
-    cdir.inner.hashOffset = cdir.inner.identOffset + @intCast(u32, id.len) + 1;
+    cdir.inner.hashOffset = cdir.inner.identOffset + @intCast(u32, cdir.data.items.len);
     var i: usize = 0;
     while (i < total_pages) : (i += 1) {
         const fstart = i * page_size;
@@ -129,9 +292,23 @@ pub fn calcAdhocSignature(
     // 3. Update CodeDirectory length
     cdir.inner.length += @intCast(u32, cdir.data.items.len);
 
-    self.inner.length += @sizeOf(macho.BlobIndex) + cdir.size();
-    self.inner.count = 1;
-    self.cdir = cdir;
+    self.inner.length += @sizeOf(macho.BlobIndex) + cdir.inner.length;
+
+    try self.blobs.append(allocator, .{ .code_directory = cdir });
+    self.inner.count += 1;
+
+    try self.blobs.append(allocator, .{ .requirements = req });
+    self.inner.count += 1;
+    self.inner.length += @sizeOf(macho.BlobIndex) + req.size();
+
+    try self.blobs.append(allocator, .{ .entitlements = ent });
+    self.inner.count += 1;
+    self.inner.length += @sizeOf(macho.BlobIndex) + ent.size();
+
+    var sig: Signature = .{};
+    try self.blobs.append(allocator, .{ .signature = sig });
+    self.inner.count += 1;
+    self.inner.length += @sizeOf(macho.BlobIndex) + sig.size();
 }
 
 pub fn size(self: CodeSignature) u32 {
@@ -140,26 +317,28 @@ pub fn size(self: CodeSignature) u32 {
 
 pub fn write(self: CodeSignature, writer: anytype) !void {
     try self.writeHeader(writer);
-    const offset: u32 = @sizeOf(macho.SuperBlob) + @sizeOf(macho.BlobIndex);
-    try writeBlobIndex(macho.CSSLOT_CODEDIRECTORY, offset, writer);
-    try self.cdir.?.write(writer);
+    var offset: u32 = @sizeOf(macho.SuperBlob) + @sizeOf(macho.BlobIndex) * @intCast(u32, self.blobs.items.len);
+    for (self.blobs.items) |blob| {
+        try writer.writeIntBig(u32, blob.slotType());
+        try writer.writeIntBig(u32, offset);
+        offset += blob.size();
+    }
+    for (self.blobs.items) |blob| {
+        try blob.write(writer);
+    }
 }
 
 pub fn deinit(self: *CodeSignature, allocator: Allocator) void {
-    if (self.cdir) |*cdir| {
-        cdir.data.deinit(allocator);
+    for (self.blobs.items) |*blob| {
+        blob.deinit(allocator);
     }
+    self.blobs.deinit(allocator);
 }
 
 fn writeHeader(self: CodeSignature, writer: anytype) !void {
     try writer.writeIntBig(u32, self.inner.magic);
     try writer.writeIntBig(u32, self.inner.length);
     try writer.writeIntBig(u32, self.inner.count);
-}
-
-fn writeBlobIndex(tt: u32, offset: u32, writer: anytype) !void {
-    try writer.writeIntBig(u32, tt);
-    try writer.writeIntBig(u32, offset);
 }
 
 test "CodeSignature header" {
@@ -178,6 +357,6 @@ pub fn calcCodeSignaturePaddingSize(id: []const u8, file_size: u64, page_size: u
     const ident_size = id.len + 1;
     const total_pages = mem.alignForwardGeneric(u64, file_size, page_size) / page_size;
     const hashed_size = total_pages * hash_size;
-    const codesig_header = @sizeOf(macho.SuperBlob) + @sizeOf(macho.BlobIndex) + @sizeOf(macho.CodeDirectory);
+    const codesig_header: u32 = @sizeOf(macho.SuperBlob) + 4 * @sizeOf(macho.BlobIndex) + @sizeOf(macho.CodeDirectory) + 0x4000;
     return @intCast(u32, mem.alignForwardGeneric(u64, codesig_header + ident_size + hashed_size, @sizeOf(u64)));
 }
